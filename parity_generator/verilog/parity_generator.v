92c789a7360e5354ede5b1bd7fa2d1e61c9eae7f
Nadimpalli Pavan Kumar <pavan39478@gmail.com>
2023-05-16 19:58:27 +0530

    added parity gen code

--- parity_generator/verilog/parity_generator.v
+++ parity_generator/verilog/parity_generator.v
@@ -1,8 +1,17 @@
-module parity_generator(
-
-
+`define EVEN_PARITY 
+module parity_generator
+#(parameter #BUS_WIDTH = 8)
+(
+input [BUS_WIDTH-1:0] inp,
+output                parity
 
 );
 
+
+#`ifdef EVEN_PARITY
+assign parity = ^inp;  //Bit-wise XOR for Even pairty bit generation. Same is used for parity checking
+#else
+assign parity = ~^inp;
+#`endif macro
 
 endmodule
