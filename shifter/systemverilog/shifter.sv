396729af9dc47dce2d4dee780402b3c3b07a9ec1
Nadimpalli Pavan Kumar <pavan39478@gmail.com>
2023-07-01 12:07:01 +0530

    added more tests to shifter tb

--- shifter/systemverilog/shifter.sv
+++ shifter/systemverilog/shifter.sv
@@ -19,13 +19,17 @@ logic[2*(DATA_WIDTH-1):0]       wider_shift_input; //2N-1 wide shifter input
 logic[2*(DATA_WIDTH-1):0]       tmp_shift_output;//2N-1 wide shifter input
 logic[$clog2(DATA_WIDTH)-1:0]   adj_shift_amount;
 logic                           sign_bit;
+logic[DATA_WIDTH-2:0]           zero_vec;
+
+assign zero_vec = {XMSB{1'b0}};
+
 
 
 assign sign_bit = shift_in_i[XMSB] & arith_shift_i; //sign bit only needed for arith shift
 
 
 always_comb //form the 2N-1 wide shift input based on the shift direction
-    if(left_shift_i)    wider_shift_input = {shift_in_i, 31'b0};
+    if(left_shift_i)    wider_shift_input = {shift_in_i, zero_vec};
     else                wider_shift_input = {{XMSB{sign_bit}}, shift_in_i};
 
 assign adj_shift_amount = left_shift_i ? ~shift_amount_i : shift_amount_i; 
--- shifter/systemverilog/tb_shifter.sv
+++ shifter/systemverilog/tb_shifter.sv
@@ -16,19 +16,58 @@ shifter #(32) shifter_dut
 .shift_out_o    //shifter output
 );
 
+logic[7:0] pass_count, fail_count;
 
 initial begin 
+    pass_count = 0;
+    fail_count = 0;
    repeat(100)
       begin
-         run_shift_stimulus();                                //instantiating task
-         if (shift_out_t == shift_out_o)  //condition to check DUT outputs and calculated 
-                                                    //outputs from task are equal 
-            $display("Test case Passed");     
-
+         run_shift_stimulus(0,1);         //logical left shift
+         if (shift_out_t == shift_out_o)
+            pass_count++;
          else
-            $display("Test case Failed");     
-      
+            fail_count++;    
       end
+      $display("[Logical Left Shift]Number of Test cases Passed = %0d", pass_count);     
+      $display("[Logical Left Shift]Number of Test cases Failed = %0d", fail_count);
+      
+      pass_count = 0;fail_count=0;
+      repeat(100)
+      begin
+         run_shift_stimulus(0,0);         //logical Right shift
+         if (shift_out_t == shift_out_o)
+            pass_count++;
+         else
+            fail_count++;    
+      end
+      $display("[Logical Right Shift]Number of Test cases Passed = %0d", pass_count);     
+      $display("[Logical Right Shift]Number of Test cases Failed = %0d", fail_count);
+      
+      pass_count = 0;fail_count=0;
+      repeat(100)
+      begin
+         run_shift_stimulus(1,1);         //Arithmetic Left shift
+         if (shift_out_t == shift_out_o)
+            pass_count++;
+         else
+            fail_count++;    
+      end
+      $display("[Arithmetic Left Shift]Number of Test cases Passed = %0d", pass_count);     
+      $display("[Arithmetic Left Shift]Number of Test cases Failed = %0d", fail_count);
+      
+      pass_count = 0;fail_count=0;
+      repeat(100)
+      begin
+         run_shift_stimulus(1,0);         //Arithmetic Right shift
+         if (shift_out_t == shift_out_o)
+            pass_count++;
+         else
+            fail_count++;    
+      end
+      $display("[Arithmetic Right Shift]Number of Test cases Passed = %0d", pass_count);     
+      $display("[Arithmetic Right Shift]Number of Test cases Failed = %0d", fail_count);
+      
       $finish;
 end
 
@@ -36,21 +75,25 @@ task run_shift_stimulus;
    
 logic[31:0] shift_in_t;
 logic[4:0] shift_amount_t;
-logic arith_shift_t, left_shift_t;
+input logic arith_shift_t, left_shift_t;
 
    begin 
    shift_in_t   = $random;   //random input
    shift_amount_t   = $random;   //random input
-   arith_shift_t  = 0;
-   left_shift_t = 0;
-
     
    shift_in_i   = shift_in_t;         
    shift_amount_i   = shift_amount_t;        
    arith_shift_i = arith_shift_t;
    left_shift_i = left_shift_t;         
-    
-   shift_out_t = shift_in_i >> shift_amount_t;#5; 
+   
+   if(arith_shift_t ==0 & left_shift_t == 1)begin //logical left shift 
+   shift_out_t = (shift_in_i) << shift_amount_t;#5;end 
+   else if(arith_shift_t ==0 & left_shift_t == 0)begin //logical right shift 
+   shift_out_t = (shift_in_i) >> shift_amount_t;#5;end
+    else if(arith_shift_t ==1 & left_shift_t == 1)begin //arithmetic left shift 
+   shift_out_t = $signed(shift_in_i) <<< shift_amount_t;#5;end
+    else if(arith_shift_t ==1 & left_shift_t == 0)begin //arithmetic right shift 
+   shift_out_t = $signed(shift_in_i) >>> shift_amount_t;#5;end
    
    end
     
